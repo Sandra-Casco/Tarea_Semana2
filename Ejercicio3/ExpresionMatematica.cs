40597cfeffbab6f963a4310f50f632ca44ab0b93
Sandra-Casco <93898744+Sandra-Casco@users.noreply.github.com>
2023-01-26 19:53:43 -0600

    Add files via upload

---
+++ Ejercicio3/ExpresionMatematica.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class ExpresionMatematica
+    {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+
+        public double suma()
+        {
+            return num1 + num2;
+        }
+
+        public double resta()
+        {
+            return num1 - num2;
+        }
+
+        public double multiplicacion()
+        {
+            return num1 * num2;
+        }
+
+        public double division()
+        {
+            return num1 / num2;
+        }
+
+        public double max()
+        {
+            if(num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+
+        public double min()
+        {
+            if (num1 < num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+    }
+}
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,38 @@
+﻿using Ejercicio3;
+using System.Collections.Generic;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Expresiones Matemáticas");
+        
+
+        ExpresionMatematica EM1 = new ExpresionMatematica()
+        {
+            num1 = 5,
+            num2 = 15,
+        };
+
+        ExpresionMatematica EM2 = new ExpresionMatematica()
+        {
+            num1 = 3,
+            num2 = 24,
+        };
+
+        List<ExpresionMatematica> listEM = new List<ExpresionMatematica>();
+
+        listEM.Add(EM1);
+        listEM.Add(EM2);
+
+        foreach(ExpresionMatematica e in listEM)
+        {
+            Console.WriteLine($"Suma: {e.suma()}");
+            Console.WriteLine($"Resta: {e.resta()}");
+            Console.WriteLine($"Multiplicación: {e.multiplicacion()}");
+            Console.WriteLine($"Division: {e.division()}");
+            Console.WriteLine($"Máximo: {e.max()}");
+            Console.WriteLine($"Mínimo: {e.min()}");
+        }
+    }
+}
